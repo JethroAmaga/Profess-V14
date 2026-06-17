330495a58e72677328593b3565286580aa81619c
JethroAmaga <jethroargabnr@gmail.com>
2026-06-17 10:42:12 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2045,7 +2045,7 @@ export default function Profess() {
   {/* Spacer untuk menambah area scroll di ujung kiri */}
   <div
     style={{
-      width:"80px",
+      width:"60px",
       flexShrink:0
     }}
   />
