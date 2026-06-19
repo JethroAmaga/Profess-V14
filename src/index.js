9fd4a1f3fdf8620b62cdaa947b935fec120aac50
JethroAmaga <jethroargabnr@gmail.com>
2026-06-20 00:12:30 +0700

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,3 @@
+export { SKIN, HAIR, ETHNICITIES, pick, ROLE_TITLES, NAME_POOL, generateChar, CHARS, MOOD_DATA, darken, lighten } from "./characterData.js";
+export { buildSVG } from "./buildSVG.js";
+export { getAnimationState, getIdleAnimation, getMoodAnimation, getPresenceMood } from "./animationState.js";
